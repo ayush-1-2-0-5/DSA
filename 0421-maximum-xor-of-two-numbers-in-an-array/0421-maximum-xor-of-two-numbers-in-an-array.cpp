653555860220d80ef36517c531560f07d4587c72
ayush-1-2-0-5 <112538276+ayush-1-2-0-5@users.noreply.github.com>
2024-06-12 16:30:11 +0530

    Time: 600 ms (52.14%), Space: 182.7 MB (42.05%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,69 @@
+struct Node {
+   Node* links[2]; 
+   bool containsKey(int bit) {
+       return (links[bit] != NULL); 
+   }
+   Node* get(int bit) {
+       return links[bit]; 
+   }
+   void put(int bit, Node* node) {
+       links[bit] = node; 
+   }
+};
+
+class Trie {
+private:
+   Node* root;
+public:
+   Trie() {
+       root = new Node();
+   }
+   void insert(int num) {
+       Node* node = root;
+       for (int i = 31; i >= 0; i--) {
+           int bit = (num >> i) & 1;
+           if (!node->containsKey(bit)) {
+               node->put(bit, new Node());
+           }
+           node = node->get(bit);
+       }
+   }
+   int getMax(int num) {
+       Node* node = root;
+       int maxNum = 0;
+       for (int i = 31; i >= 0; i--) {
+           int bit = (num >> i) & 1;
+           if (node->containsKey(1 - bit)) {
+               maxNum |= (1 << i);
+               node = node->get(1 - bit);
+           } else {
+               node = node->get(bit);
+           }
+       }
+       return maxNum;
+   }
+};
+
+class Solution {
+public:
+   int findMaximumXOR(vector<int>& nums) {
+       int n=nums.size();
+       int maxi=INT_MIN;
+
+       Trie trie;
+
+       for (auto it: nums)
+       {
+        trie.insert(it);
+       }
+
+       for(auto it: nums)
+       {
+          int a=trie.getMax(it);
+          maxi=max(maxi,a);
+       }
+         return maxi;
+   }
+};
+
+

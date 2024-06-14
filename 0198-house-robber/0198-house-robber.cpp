43ac280ed2d846518777c51bc68da2630914baf6
ayush-1-2-0-5 <112538276+ayush-1-2-0-5@users.noreply.github.com>
2024-06-14 15:01:35 +0530

    Time: 2 ms (40.24%), Space: 9.2 MB (59.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int f(int ind, vector<int>& nums, vector<int>& dp) {
-        if (ind < 0)
-            return 0;
-        if (dp[ind] != -1)
-            return dp[ind];
+    int f(int ind,vector<int> &nums,vector<int> &dp)
+    {
+        if(ind<0)
+        return 0;
+        if(dp[ind]!=-1)
+        return dp[ind];
 
-        int include = nums[ind] + f(ind - 2, nums, dp);
-        int exclude = f(ind - 1, nums, dp);
+        int include=nums[ind]+f(ind-2,nums,dp);
+        int exclude=f(ind-1,nums,dp);
 
-        return dp[ind] = max(include, exclude);
+       return dp[ind]=max(include,exclude);
     }
-
     int rob(vector<int>& nums) {
-        int n = nums.size();
+       int n = nums.size();
         if (n == 1)
             return nums[0];
         if (n == 2)
@@ -26,5 +26,6 @@ public:
         int b = f(n - 1, nums, dp2);  
 
         return max(a,b);
+        
     }
 };

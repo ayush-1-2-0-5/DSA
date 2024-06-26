3090b995043c3bc18a5d9b8653cf8bff10077931
ayush-1-2-0-5 <112538276+ayush-1-2-0-5@users.noreply.github.com>
2024-06-27 01:18:35 +0530

    Time: 0 ms (100%), Space: 7 MB (77.38%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.cpp
+++ 2507-number-of-common-factors/2507-number-of-common-factors.cpp
@@ -1,29 +1,10 @@
-
 class Solution {
 public:
     int commonFactors(int a, int b) {
-        int ct = 0;
-        int d = min(a, b);
-
-        for (int i = 1; i*i <= d; ++i) {
-            if (d%i==0)
-            {
-                int f=i;
-                int sf=d/i;
-                 if(f==sf)
-                 {
-                    if(a%i==0 && b%i==0)
-                    ct++;
-                 }
-                 else
-                 {
-                       if(a%i==0 && b%i==0)
-                    ct++;
-                       if(a%sf==0 && b%sf==0)
-                    ct++;
-                 }
-            }
+        int cnt=0;
+        for(int i=1;i<=min(a,b);i++){
+          if(a%i==0 && b%i==0)cnt++;
         }
-        return ct;
+        return cnt;
     }
 };

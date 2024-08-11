c133420586eae44dabff429f44b9528c73c5f0fa
ayush-1-2-0-5 <112538276+ayush-1-2-0-5@users.noreply.github.com>
2024-08-11 12:05:07 +0530

    Time: 0 ms (100%), Space: 8.3 MB (13.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        map<char, char> mp = {{')', '('}, {']', '['}, {'}', '{'}};
+
+        for (auto i : s) {
+            if (i == '(' || i == '[' || i == '{') {
+                st.push(i);
+            } else {
+                if (st.empty() || st.top() != mp[i]) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty(); // Ensure all brackets are matched
+    }
+};

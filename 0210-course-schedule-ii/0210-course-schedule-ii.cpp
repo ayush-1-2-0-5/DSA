a536498038bdf2b924182bf7bd587581f2f7dde5
ayush-1-2-0-5 <112538276+ayush-1-2-0-5@users.noreply.github.com>
2024-08-25 19:34:27 +0530

    Time: 11 ms (91.01%), Space: 17.5 MB (63.08%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,48 +1,51 @@
 class Solution {
 public:
-    vector<int> findOrder(int V, vector<vector<int>>& pre) {
-         vector<int> indegree(V,0);
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int> indegree(n,0);
 
-         vector<int> adj[V];
-         for(auto it:pre )
-         {
+        vector<int> adj[n];
+
+        for(auto it: pre)
+        {
             adj[it[1]].push_back(it[0]);
-         }
-	  
-	  for(int i=0;i<V;++i)
-	  {
-	      for(auto it:adj[i])
-	      {
-	          indegree[it]++;
-	      }
-	  }
-	  
-	  queue<int> q;
-	  for(int i=0;i<V;++i)
-	  {
-	      if(indegree[i]==0)
-	      q.push(i);
-	  }
-	  vector<int> ans;
-	  while(!q.empty())
-	  {
-	      int a=q.front();
-	      ans.push_back(a);
-	      q.pop();
-	      
-	      for(auto it:adj[a])
-	      {
-	          indegree[it]--;
-	          if(indegree[it]==0)
-	          q.push(it);
-	      }
-	      
-	      
-	      
-	  }
-	  if(ans.size()==V)
-    return ans;
-    else 
-    return{};
+        }
+
+
+        for(int i=0;i<n;++i)
+        {
+            for(auto it: adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+
+        for(int i=0;i<n;++i)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+
+        while(!q.empty())
+        {
+            int a=q.front();
+            ans.push_back(a);
+            q.pop();
+
+            for(auto it:adj[a])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                q.push(it);
+            }
+        }
+        if(ans.size()==n)
+        return ans;
+        else return {};
     }
 };

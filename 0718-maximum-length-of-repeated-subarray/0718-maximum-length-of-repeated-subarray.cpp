95ef7a0749b63d80208dcbd41452cd865383cdc9
ayush-1-2-0-5 <112538276+ayush-1-2-0-5@users.noreply.github.com>
2024-09-11 15:58:41 +0530

    Time: 78 ms (67.03%), Space: 88.3 MB (16.57%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -7,16 +7,16 @@ public:
         int n = nums1.size();
         int m = nums2.size();
         
-        std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, 0));
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
         int maxLength = 0;
         
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= m; ++j) {
                 if (nums1[i - 1] == nums2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                    maxLength = std::max(maxLength, dp[i][j]);
+                    maxLength = max(maxLength, dp[i][j]);
                 } else {
-                    dp[i][j] = 0;  // Not necessary but makes intention clear
+                    dp[i][j] = 0;  
                 }
             }
         }

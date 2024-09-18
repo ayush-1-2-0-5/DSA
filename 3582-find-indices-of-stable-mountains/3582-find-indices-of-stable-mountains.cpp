ad2d745ebe5a1a21f64f78c1dce2488e386b41b7
ayush-1-2-0-5 <112538276+ayush-1-2-0-5@users.noreply.github.com>
2024-09-18 15:03:48 +0530

    Time: 13 ms (11.76%), Space: 27.4 MB (94.36%) - LeetHub

---
+++ 3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define fr(a,b) for(int i = a; i < b; i++)
+#define frn(a,b) for(int i = a; i >= b; i--)
+#define frr(a,b,c) for(int i = a; i <= b &&c; i++)
+#define frrn(a,b,c) for(int i = a; i >=b &&c; i--)
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+#define prDouble(x) cout << fixed << setprecision(10) << x
+#define triplet pair<ll,pair<ll,ll>>
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+class Solution {
+public:
+    vector<int> stableMountains(vector<int>& h, int t) {
+        vi ans;
+        int n=h.size();
+
+        fr(1,n)
+        {
+            if(h[i-1]>t)
+            ans.pb(i);
+        }
+
+        return ans;
+    }
+};

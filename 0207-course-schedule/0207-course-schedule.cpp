1cc8d011df27eefdf30e1cd72cd4e04a8a1cc302
ayush-1-2-0-5 <112538276+ayush-1-2-0-5@users.noreply.github.com>
2025-01-25 00:10:54 +0530

    Time: 14 ms (15.05%), Space: 20 MB (49.32%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& pre) {
-        vector<int> adj[numCourses];
+        vector<vector<int>> adj(numCourses);
+        
+        // Build adjacency list
         for(auto it: pre) {
             adj[it[1]].push_back(it[0]);
         }
+        
+        // Calculate in-degrees of each course
         vector<int> indegree(numCourses, 0);
-        for(int i = 0; i < pre.size(); ++i) {
-            indegree[pre[i][0]]++;
+        for(int i = 0; i < numCourses; ++i) {
+            for(auto it: adj[i]) {
+                indegree[it]++;
+            }
         }
 
         queue<int> q;
-
         for(int i = 0; i < numCourses; ++i) {
             if(indegree[i] == 0) {
                 q.push(i);
@@ -23,6 +28,7 @@ public:
             int curr = q.front();
             q.pop();
             cnt++;
+
             for(auto next: adj[curr]) {
                 indegree[next]--;
                 if(indegree[next] == 0) {
@@ -30,6 +36,8 @@ public:
                 }
             }
         }
+
         return cnt == numCourses;
+    
     }
 };
